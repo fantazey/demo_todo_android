c073b6a8ad78241d2c4bfc8d6eae0718b0053599
Andrey Dyatlov <fantazey@gmail.com>
2024-09-26 23:44:27 +0300

    add view bindings

--- app/src/main/java/com/example/firsttestapp/AuthFragment.kt
+++ app/src/main/java/com/example/firsttestapp/AuthFragment.kt
@@ -10,26 +10,36 @@ import android.widget.EditText
 import android.widget.TextView
 import androidx.navigation.findNavController
 
+import com.example.firsttestapp.databinding.FragmentAuthBinding
+
 
 class AuthFragment : Fragment() {
+    private var _binding: FragmentAuthBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_auth, container, false)
-        val signInButton = view.findViewById<Button>(R.id.singin)
-
+        _binding = FragmentAuthBinding.inflate(inflater, container, false)
+        val view = binding.root
+        val signInButton = binding.singin
         signInButton.setOnClickListener {
-            val login = view.findViewById<EditText>(R.id.login)
-            val password = view.findViewById<EditText>(R.id.password)
+            val login = binding.login
+            val password = binding.password
             if (password.text.toString() ==  login.text.toString()) {
                 val action = AuthFragmentDirections.actionSuccessLogin(login.text.toString())
                 view.findNavController().navigate(action)
             } else {
-                val textView = view.findViewById<TextView>(R.id.result)
+                val textView = binding.result
                 textView.visibility = View.VISIBLE
             }
         }
         return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
